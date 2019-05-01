b9553ce200455ca8017ef3b71064d99c22ee52dc
Shinpei Abe <shinpei@StartToday-2.local>
2019-05-01 13:00:44 +0900

    first

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,30 @@
+class PostsController < ApplicationController
+
+  before_action :logout_move_index,  except: :index
+  
+  def index
+    @posts = Post.all.order('created_at DESC').page(params[:page]).per(10)
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:title, :text, :image)
+  end
+
+  def logout_move_index
+    # edirect_to action: :index unless user_signed_in?
+  end
+end
